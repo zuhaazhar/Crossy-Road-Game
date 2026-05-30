1191850a6ae4a33cf22b46c388d60771179e6aee
Zuha Azhar <zuhazhar19@gmail.com>
2026-05-30 15:47:21 +0500

    Refactor Lane class for texture and sprite management
    Refactored Lane class to manage textures and sprites, added copy constructor and assignment operator.

--- code/src/lane.cpp
+++ code/src/lane.cpp
@@ -1,22 +1,71 @@
-#include "C:\Users\Hp EliteBook 840 G5\Documents\OOP\CCP\code\include\lane.h"
+#include "lane.h"
+#include <cstdio>
 
-Lane::Lane(float x, float y, float width, float tileSize, Type t) {
-    laneType = t;
-    shape.setSize({width, tileSize});
-    shape.setPosition({x, y});
+std::string Lane::textureFor(Type t) {
+    switch (t) {
+        case SAFE:  return "..\\assets\\images\\safe.png";
+        case ROAD:  return "..\\assets\\images\\road.png";
+        case RIVER: return "..\\assets\\images\\river.png";
+        default:    return "..\\assets\\images\\safe.png";
+    }
+}
 
-    if (t == SAFE)
-        shape.setFillColor(Color(34, 139, 34));
-    else if (t == ROAD)
-        shape.setFillColor(Color(80, 80, 80));
-    else if (t == RIVER)
-        shape.setFillColor(Color(30, 100, 200));
+Lane::Lane(float x, float y, float width, float tileSize, Type t)
+    : laneType(t), y(y), w(width), h(tileSize) {
+
+    bool loaded = texture.loadFromFile(textureFor(t));
+    if (!loaded) {
+        printf("LANE MISSING: %s\n", textureFor(t).c_str());
+        fflush(stdout);
+        sf::Image img({1, 1}, sf::Color::White);
+        texture.loadFromImage(img);
+    }
+
+    sprite = new Sprite(texture);
+    Vector2u texSize = texture.getSize();
+    if (texSize.x > 0 && texSize.y > 0)
+        sprite->setScale({width / (float)texSize.x, tileSize / (float)texSize.y});
+    sprite->setPosition({x, y});
+}
+
+Lane::Lane(const Lane& other)
+    : laneType(other.laneType), y(other.y), w(other.w), h(other.h),
+      texture(other.texture) {
+    sprite = new Sprite(texture);
+    sprite->setScale(other.sprite->getScale());
+    sprite->setPosition(other.sprite->getPosition());
+}
+
+Lane::~Lane() {
+    delete sprite;
 }
 
 void Lane::draw(RenderWindow& window) {
-    window.draw(shape);
+    window.draw(*sprite);
 }
 
-Lane::Type Lane::getLaneType() {
+Lane::Type Lane::getLaneType() const {
     return laneType;
+}
+
+float Lane::getY() const {
+    return y;
+}
+Lane& Lane::operator=(const Lane& other) {
+    if (this != &other) {
+        delete sprite;
+        laneType = other.laneType;
+        y = other.y;
+        w = other.w;
+        h = other.h;
+        texture = other.texture;
+        sprite = new Sprite(texture);
+        sprite->setScale(other.sprite->getScale());
+        sprite->setPosition(other.sprite->getPosition());
+    }
+    return *this;
+}
+
+FloatRect Lane::getBounds() const {
+    return sprite->getGlobalBounds();
 }
